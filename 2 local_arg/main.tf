8def48668a93a16b6166fa74f5daa00c388b4874
satishj3 <48849071+satishj3@users.noreply.github.com>
2022-11-30 12:13:36 +0530

    Delete 2 local_arg directory

--- 2 local_arg/main.tf
+++
@@ -1,8 +0,0 @@
-resource local_file name {
-  #sensitive_content = ""
-  # filename             = "${path.module}/files/outputfile"
-  filename             = "sample_arg.txt"
-  sensitive_content              = "I love Terraform from sentive contect"
-  # file_permission      = 0777
-  # directory_permission = 0777
-}
